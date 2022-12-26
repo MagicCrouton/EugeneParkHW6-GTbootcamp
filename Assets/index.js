eff323fdbee9e99d210550c77456f5e443f64231
Eugene Park <e.park5336@gmail.com>
2022-12-26 16:46:18 -0500

    got each of the api calls outputting code into variables

--- Assets/index.js
+++ Assets/index.js
@@ -8,33 +8,48 @@
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const API = localStorage.getItem('weatherAPI');
-var feed = {  
-                lat: 'temp',
-                lon: 'temp'};
 
+var coordinates = {  
+  lat: 'temp',
+  lon: 'temp'};             
 
+const currentWeather = {};
+var fiveDayWeather = [];
 function geoCodeFetch (city, state){
+
    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},USA&limit=5&appid=${API}`)
   .then((response) => response.json())
   .then((data) => {
-    feed.lat = data[0].lat;
-    feed.lon = data[0].lon;
+    coordinates.lat = data[0].lat;
+    coordinates.lon = data[0].lon;
     console.log(data);
-    // console.log(`lat: ${lat} lon: ${lon}`)
   })
 }
 
 function currentWeatherFetch (lat, lon){
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`)
  .then((response) => response.json())
- .then((data) => console.log(data)); 
+ .then((data) => {
+  currentWeather.weatherDescrip = data.weather[0].description;
+  currentWeather.temperature = data.main.temp;
+  currentWeather.maxTemp = data.main.temp_max;
+  currentWeather.minTemp = data.main.temp_min;
+  currentWeather.humidity = data.main.humidity;
+  currentWeather.pressure = data.main.pressure;
+  currentWeather.speed = data.wind.speed;
+  currentWeather.windDeg = data.wind.deg;
+ }); 
 }
 
 function fiveDayFetch (lat, lon){
-  fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`)
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`)
  .then((response) => response.json())
- .then((data) => console.log(data)); 
-}
+ .then((data) => {
+  console.log(data);
+  for (i=0; i < data.list.length; i++){
+    fiveDayWeather[i] = data.list[i];
+  }});
+ }
 
 
 
