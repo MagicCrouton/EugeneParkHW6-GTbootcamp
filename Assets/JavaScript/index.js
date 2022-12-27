20ea4e0ae7f4abcfdd343e5bb6bfabe4dee1813c
Eugene Park <e.park5336@gmail.com>
2022-12-27 01:43:35 -0500

    async function is now properly working

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -1,28 +1,30 @@
 // Variable define the weather key is stored in local storage as weatherAPI
 const API = localStorage.getItem('weatherAPI');
+const fiveDayWeather = [];
 const coordinates = {};
 const currentWeather = {};
-const fiveDayWeather = [];
-const itemList = [];
 const userSubmitBtn = $('#submitBtn');
 const cityInputEl = $('#cityInput');
 const userInputEl = $('#userInput');
+var workingIndex = '';
 
 
 function geoCodeFetch (city, state){
-   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},USA&limit=5&appid=${API}`)
+   return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},USA&limit=5&appid=${API}`)
   .then((response) => response.json())
   .then((data) => {
+    // const coordinates = [];
     coordinates.lat = data[0].lat;
     coordinates.lon = data[0].lon;
-    console.log(data);
+    return coordinates
   })
 }
 
 function currentWeatherFetch (lat, lon){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`)
  .then((response) => response.json())
  .then((data) => {
+  // const currentWeather ={};
   currentWeather.weatherDescrip = data.weather[0].description;
   currentWeather.temperature = data.main.temp;
   currentWeather.maxTemp = data.main.temp_max;
@@ -31,44 +33,24 @@ function currentWeatherFetch (lat, lon){
   currentWeather.pressure = data.main.pressure;
   currentWeather.speed = data.wind.speed;
   currentWeather.windDeg = data.wind.deg;
+  return currentWeather
  }); 
 }
 
 function fiveDayFetch (lat, lon){
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}`)
  .then((response) => response.json())
  .then((data) => {
+  console.log(data)
   for (i=0; i < data.list.length; i++){
     fiveDayWeather[i] = data.list[i];
-  }});
+  }
+});
  }
-
-// for (i=0; i < cityData.length; i++) {
-//   cityList[i] = cityData[i].City;
-// }
-// City list is just the city names taken out of the cityData array this is used for autocomplete and indexing in the object array.
-// jquery autocomplete UI
-//  $("#cityInput").autocomplete({
-//   source : cityList 
-// })
-
-// for (i=0; i < cityData.length; i++) {
-//   $('#cityTable').append(
-//     `
-//     <tr>
-//     <td>${cityData[i].City}</td>
-//     <td>${cityData[i].State}</td>
-//     </tr>
-//     `
-//   )
-// }
-
-
 
 userSubmitBtn.on('click', function(event){
   // event.preventDefault();
   $('#cityTable').empty();
-
   $('#cityTable').append(`
               <tr class="tableHeader">
               <th style="width :60%">City</th>
@@ -84,16 +66,32 @@ userSubmitBtn.on('click', function(event){
       $('#cityTable').append(
         `
         <tr id = "cityDataIndex${i}" class = "filteredResults">
-        <td>${cityData[i].City}</td>
-        <td>${cityData[i].State}</td>S
+        <td><a id ="${i}" class = "userClick" href="">${cityData[i].City}</a></td>
+        <td>${cityData[i].State}</td>
         </tr>
         `
       )
     }
     i = i + 1;
   });
-  // filter(cityData, userInputEl.val());
-})
+ let userClickEl = $('.userClick');
+  userClickEl.on('click', function(event){
+    event.preventDefault();
+    let temp = $(`#${event.target.id}`);
+    console.log(temp.text())
+    workingIndex = cityData.findIndex((element) => element.City === temp.text());
+    let city = cityData[workingIndex].City;
+    let state = cityData[workingIndex].State;
+    async function api(){
+      await geoCodeFetch(city,state).then((data) => console.log(data));
+      await currentWeatherFetch(coordinates.lat, coordinates.lon).then((data) => console.log(data));
+      await fiveDayFetch(coordinates.lat, coordinates.lon).then((data) => console.log(data));
+    }
+    api();
+  })
+  });
+
+ 
 
 
 
